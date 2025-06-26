e5ab7692208a1a0afaaec9adf1f050a3f9a5a244
lorenzosalti <lorenzosalti@gmail.com>
2025-06-26 15:11:39 +0200

    add: public properties to Prodotto

--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -2,4 +2,10 @@ package org.lessons.java.shop;
 
 public class Prodotto {
 
+  public int code;
+  public String name;
+  public String description;
+  public float netPrice;
+  public int iva;
+
 }
