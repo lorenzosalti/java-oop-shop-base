d85f2bad4473960929a02e5300ea1532ef3c382c
lorenzosalti <lorenzosalti@gmail.com>
2025-06-26 15:38:53 +0200

    feat: import, creation and print of a new Prodotto

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -1,7 +1,16 @@
 package org.lessons.java.shop;
 
+import org.lessons.java.shop.Prodotto;
+
 public class Main {
   public static void main(String[] args) {
 
+    Prodotto milk = new Prodotto("Parmalat", "Latte di vacca parzialmente scremato", 1.25f, 4);
+
+    System.out.println(milk.name);
+    System.out.println(milk.description);
+    System.out.println(milk.netPrice);
+    System.out.println(milk.iva);
+    System.out.println(milk.code);
   }
 }
